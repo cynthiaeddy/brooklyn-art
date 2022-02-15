e7454157c54af702db648509c023beaa7d0510ec
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-02-14 23:31:25 -0500

    getting warhol without term, cleaning up

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -4,51 +4,18 @@ const axios = require('axios')
 const SearchResults = props => {
   const [article, setArticle] = useState([])
   const [title, setTitle] = useState([])
+  const [artistId, setArtistId] = useState([])
 
   const [isSearching, setIsSearching] = useState(false)
   console.log('props.term', props.term)
 
   useEffect(() => {
-    const term = props.term
-    console.log('in useeffect', `/api`)
-    // if (term) {
-    setIsSearching(true)
-    axios.get(`/api`).then(response => {
-      // setArticles(
-      // console.log(
-      //   response.data.objects[0].title,
-      //   response.data.objects[0].images,
-      //   response.data.objects[0].images[0].b.url
-      // )
-      setArticle(response.data.objects[0].images[0].b.url)
-      setTitle(response.data.objects[0].title)
+    axios.get('api').then(response => {
+      console.log(response.data)
     })
-    // console.log(response.data, response.data.objects[0].images[0])
-    // })
+  }, [])
 
-    // setIsSearching(false)
-  }, [props.term])
-  // const img = articles && articles.images[0].b.url
-  // const title = articles.title
-  // console.log(articles.images)
-  // let img = articles ? articles.images[0].b.url : 'loading'
-  // }
-  // const img = articles.filter(article => article.url[0])
-  // console.log(img)
-  return (
-    <>
-      {/* {<img src={articles && articles.images.map(image => image.b.url)} />} */}
-      <img
-        src={article}
-        className='Article-img'
-        alt=''
-        style={{
-          width: '300px',
-        }}
-      />
-      <h5>{title}</h5>
-    </>
-  )
+  return <></>
 }
 
 export default SearchResults
--- server/index copy 3.js
+++ server/index copy 3.js
@@ -7,20 +7,20 @@ const PORT = process.env.PORT || 3005
 const app = express()
 
 app.get('/api', async (req, res, next) => {
+  console.log('/api')
   const api_key = process.env.API_KEY
   const api_key_cooper = process.env.COOPER_API_TOKEN
 
-  // console.log(req.params)
-  console.log('/api:term', api_key)
-  // const term = req.params.term
-  // console.log('term', term)
+  console.log(req.params)
+  const term = req.params.term
+  console.log(term)
 
   try {
     const response = await axios.get(
       `https://www.brooklynmuseum.org/api/v2/artist?name=warhol`,
-      //   // `https://www.brooklynmuseum.org/api/v2/collection/?folder=contemporary_art`,
-      //   // `https://www.brooklynmuseum.org/api/v2/collection/?folder=contemporary_art`,
-      //   // `https://www.brooklynmuseum.org/api/v2/object?has_images=1&artist_id=2019`,
+      // `https://www.brooklynmuseum.org/api/v2/collection/?folder=contemporary_art`,
+      // `https://www.brooklynmuseum.org/api/v2/collection/?folder=contemporary_art`,
+      // `https://www.brooklynmuseum.org/api/v2/object?has_images=1&artist_id=2019`,
       {
         headers: { api_key: api_key },
       }
