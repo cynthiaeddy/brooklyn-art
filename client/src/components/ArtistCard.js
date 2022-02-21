32ccb149c891cb11c7d9a9cf7cfc203dd6661855
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-02-20 20:33:45 -0500

    align text center

--- client/src/components/ArtistCard.js
+++ client/src/components/ArtistCard.js
@@ -2,24 +2,15 @@ import React from 'react'
 
 //...
 const ArtistCard = props => {
-  console.log(props.caption, props.caption.split('.'))
-  const captionSplit = props.caption.split('.')
-  console.log(captionSplit[0], captionSplit[1].split('<em>'))
+  const captionSplit = props.caption.replace(/(<([^>]+)>)/gi, '').split('.')
+  const title = captionSplit[1]
+  const medium = captionSplit[2]
 
-  const title = captionSplit[1].replace(/(<([^>]+)>)/gi, '')
-
-  // console.log(
-  //   'inArtistCard, props.img',
-  //   props.img,
-  //   'props.caption',
-  //   props.caption,
-  //   'props',
-  //   props
-  // )
   return (
     <div className='card'>
       <div className='box'></div>
-      <h4>{title}</h4>
+      <h4 style={{ marginBottom: '0' }}>{title}</h4>
+      <h5 style={{ margin: '0' }}>{medium}</h5>
     </div>
   )
 }
