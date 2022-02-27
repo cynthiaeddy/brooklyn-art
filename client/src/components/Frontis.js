0f8ed768d4f0a21752eded47fb88d2c5dc8edd32
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-02-27 00:52:04 -0500

    added frontis animation

--- client/src/components/Frontis.js
+++ client/src/components/Frontis.js
@@ -2,7 +2,15 @@ import React from 'react'
 import '../stylesheets/Frontis.css'
 
 const Frontis = () => {
-  return <div className='frontis'>Brooklyn Art Museum Project</div>
+  return (
+    <a>
+      <div className='frontis'>
+        <div className='type'>
+          <h1>The Brooklyn Art Museum Project</h1>
+        </div>
+      </div>
+    </a>
+  )
 }
 
 export default Frontis
