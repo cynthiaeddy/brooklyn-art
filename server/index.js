04f49489aaef5605fd64234a11251ce3a21a9158
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-03-08 17:25:54 -0500

    update server/index.js

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const express = require('express')
 const axios = require('axios')
 const PORT = process.env.PORT || 3005
 const app = express()
-const publicPath = path.join(__dirname, '..', 'public')
-app.use(express.static(publicPath))
+// const publicPath = path.join(__dirname, '..', 'public')
+// app.use(express.static(publicPath))
+
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get(`/api/:term`, async (req, res, next) => {
   const api_key = process.env.API_KEY
@@ -48,7 +50,7 @@ app.get(`/api/:term`, async (req, res, next) => {
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'))
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
 app.enable('trust proxy')
