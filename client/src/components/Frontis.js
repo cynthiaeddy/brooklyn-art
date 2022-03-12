38b58d64479c5a0c894fc12d9ad5afa4fc44cc69
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-03-12 18:12:40 -0500

    restyle frontis

--- client/src/components/Frontis.js
+++ client/src/components/Frontis.js
@@ -29,7 +29,7 @@ const Frontis = () => {
       {animate && !timePassed ? (
         <div className='frontis'>
           <div className='type'>
-            <h1>The Brooklyn Art Museum Project</h1>
+            <h1>The Brooklyn Museum Project</h1>
           </div>
         </div>
       ) : null}
